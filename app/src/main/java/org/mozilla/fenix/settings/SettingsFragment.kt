9f7f260e2318d031c24388278a2f8b0eeede9d92
Valentin Heidelberger <valh@mailbox.org>
2022-08-04 13:26:12 +0200

    patch v103.2.0 and release as 103.2.0-mod

--- app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -583,9 +583,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
             findPreference<Preference>(getPreferenceKey(R.string.pref_key_override_amo_collection))
 
         val show = (
-            Config.channel.isNightlyOrDebug && (
                 settings.amoCollectionOverrideConfigured() || settings.showSecretDebugMenuThisSession
-                )
             )
         preferenceAmoCollectionOverride?.apply {
             isVisible = show
