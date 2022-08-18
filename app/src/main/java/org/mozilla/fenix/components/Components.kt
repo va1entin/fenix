25a754c817c91bc6d06f9c4daac3945c63c6e1df
Valentin Heidelberger <valh@mailbox.org>
2022-08-18 14:28:31 +0200

    patch v104.1.0 and release as 104.1.0-mod

--- app/src/main/java/org/mozilla/fenix/components/Components.kt
+++ app/src/main/java/org/mozilla/fenix/components/Components.kt
@@ -104,7 +104,7 @@ class Components(private val context: Context) {
 
     val addonCollectionProvider by lazyMonitored {
         // Check if we have a customized (overridden) AMO collection (only supported in Nightly)
-        if (Config.channel.isNightlyOrDebug && context.settings().amoCollectionOverrideConfigured()) {
+        if (context.settings().amoCollectionOverrideConfigured()) {
             AddonCollectionProvider(
                 context,
                 core.client,
